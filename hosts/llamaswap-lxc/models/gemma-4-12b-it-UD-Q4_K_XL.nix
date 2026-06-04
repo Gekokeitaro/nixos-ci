c5c880251d08bf60e5ce81ceda2de721b800bfef
PBugarin <pablo.bugarin.camara@gmail.com>
2026-06-04 17:34:09 +0200

    FEATURE - Gemm4 12b for testing

---
+++ hosts/llamaswap-lxc/models/gemma-4-12b-it-UD-Q4_K_XL.nix
@@ -0,0 +1,19 @@
+{llama-server}: {
+  "gemma-4-12b-it-UD-Q4_K_XL" = {
+    name = "gemma-4-12b-it-UD-Q4_K_XL";
+    cmd = ''
+      ${llama-server} --port ''${PORT}
+      --model /models/gemma-4-12b-it-UD-Q4_K_XL.gguf
+      --n-gpu-layers 99
+      --ctx-size 32768
+      --batch-size 512
+      --ubatch-size 512
+      --threads 1
+      --parallel 1
+      --jinja
+      --fit off
+      --no-warmup
+    '';
+    ttl = 600;
+  };
+}
