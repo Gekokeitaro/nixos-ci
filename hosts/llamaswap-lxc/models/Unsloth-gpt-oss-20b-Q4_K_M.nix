153a6f202e30d8fef9f7641aba671017b59304c8
PBugarin <pablo.bugarin.camara@gmail.com>
2026-07-01 17:08:55 +0200

    FEATURE - GPT-OSS optimized

--- hosts/llamaswap-lxc/models/Unsloth-gpt-oss-20b-Q4_K_M.nix
+++ hosts/llamaswap-lxc/models/Unsloth-gpt-oss-20b-Q4_K_M.nix
@@ -1,3 +1,5 @@
+# 010726
+# ./llama-bench --delay 10 -m /models/gpt-oss-20b-Q4_K_M.gguf -ngl 99 -t 4 -b 512 -ub 512 -ctk q8_0 -ctv q4_0 -fa 1 -ot ".ffn_(up|down)_exps.=CPU"
 {llama-server}: {
   "Unsloth GPT OSS 20B Q4_K_M" = {
     name = "Unsloth GPT OSS 20B Q4_K_M";
@@ -6,9 +8,10 @@
       --model /models/gpt-oss-20b-Q4_K_M.gguf
       --spec-type ngram-mod
       --ctx-size 32768
-      --n-gpu-layers 99
-      --threads 2
-      --n-cpu-moe 4
+      -ngl 99 -t 4
+      -b 512 -ub 512
+      -ctk q8_0 -ctv q4_0 -fa 1
+      -ot ".ffn_(up|down)_exps.=CPU"
       --parallel 1
       --no-warmup
     '';
